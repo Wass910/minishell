18f0215bc72b8a3db20e0f1e80ddb988755d3546
helaski <jesuisformule1@gmail.com>
2021-11-18 10:49:52 +0100

    bultin merge

---
+++ src/flstclear.c
@@ -0,0 +1,20 @@
+#include "../inc/minishell.h"
+
+void	flstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*tmp;
+
+	if (!(*lst))
+		return ;
+	if (del)
+	{
+		while (*lst)
+		{
+			del((*lst)->content);
+			tmp = (*lst);
+			*lst = (*lst)->next;
+			free(tmp);
+		}
+	}
+	*lst = NULL;
+}
