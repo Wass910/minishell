02c8beda492269412479bba41b23bb9749770432
mahrez91 <wassimdhibaa@gmail.com>
2021-11-19 17:26:34 +0100

    new depart

---
+++ src/flstadd_back.c
@@ -0,0 +1,16 @@
+#include "../inc/minishell.h"
+
+void	flstadd_back(t_list **alst, t_list *new)
+{
+	t_list	*begin;
+
+	if (!(*alst))
+	{
+		(*alst) = new;
+		return ;
+	}
+	begin = (*alst);
+	while (begin->next)
+		begin = begin->next;
+	begin->next = new;
+}
---
+++ src/pipex.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idhiba <idhiba@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/08 12:52:19 by idhiba            #+#    #+#             */
+/*   Updated: 2021/11/19 17:16:26 by idhiba           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minishell.h"
+
+void	pipex(t_pip *parse_pip, int nb_cmds)
+{
+	int pipefd[2];
+	pid_t pid1;
+	int i;
+
+    while (nb_cmds > 0)
+    {
+      if (pipe(parse_pip->pipefd) == -1)
+        exit(EXIT_FAILURE);
+      pid1 = fork();
+      if (pid1 == -1)
+        exit(EXIT_FAILURE);
+      if (pid1)
+      {
+		      close(parse_pip->pipefd[1]);
+          dup2(parse_pip->pipefd[0], STDIN);
+		  if (nb_cmds == 1)
+		  {
+			    dup2(STDIN, pipefd[0]);
+    	  	dup2(STDOUT,pipefd[1]);
+		  }
+        waitpid(pid1, NULL, 0);	
+      }
+      else
+      {
+        close(parse_pip->pipefd[0]);
+        if(nb_cmds != 1)
+			  dup2(parse_pip->pipefd[1], STDOUT);
+		    if (nb_cmds != 1)
+        	execve(parse_pip->path, parse_pip->cmd, NULL);
+		    else
+			    execve(parse_pip->next->path, parse_pip->next->cmd, NULL);
+		    return ;
+      }
+	  nb_cmds--;
+	  if (nb_cmds != 1)
+		  {
+				parse_pip = parse_pip->next;
+		  }
+    }
+}
+
+void	pipex_for_one(t_pip *parse_pip)
+{
+	int pipefd[2];
+	pid_t pid1;
+	int i;
+
+      if (pipe(parse_pip->pipefd) == -1)
+        exit(EXIT_FAILURE);
+      pid1 = fork();
+      if (pid1 == -1)
+        exit(EXIT_FAILURE);
+      if (pid1)
+      {
+        waitpid(pid1, NULL, 0);	
+      }
+      else
+      {
+        execve(parse_pip->path, parse_pip->cmd, NULL);
+        kill(SIGHUP, 0);
+		    return ;
+      }
+
+}
