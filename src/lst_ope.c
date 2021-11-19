a9970affdbe3766ee543a4433d255e2a95558e0d
helaski <jesuisformule1@gmail.com>
2021-11-19 10:15:31 +0100

    updated

---
+++ src/lst_ope.c
@@ -0,0 +1,12 @@
+#include "../inc/minishell.h"
+
+t_comm	fill_comm(t_comm comm ,char *cmd)
+{
+	//printf("c = %s\n", cmd);
+  	comm.nb_pipe = ft_strchr(cmd, '|');
+  	comm.redir_input = ft_strchr(cmd, '<');
+  	comm.redir_output = ft_strchr(cmd, '>');
+	comm.single_quote = ft_strchr(cmd, 39);
+  	comm.double_quote = ft_strchr(cmd, '"');
+	return (comm);
+}
